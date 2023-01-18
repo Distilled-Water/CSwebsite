fa76ee4fe21b8031ceadc54e3e6726b0da9eeb11
MetroMap <76685270+Distilled-Water@users.noreply.github.com>
2023-01-18 16:01:06 +0000

    Add files via upload

--- 7woftw.js
+++ 7woftw.js
@@ -1,15 +1,27 @@
 let n = 1;
-let darkModeOn = False;
+let darkModeOn = 1;
 
 function myFunction() {
 	n += 1;
 	document.getElementById("textBox").innerHTML = n;
 }
 
-function toggleDarkMode() {
+function toggleDarkMode(color) {
 	var element = document.body;
 	element.classList.toggle("darkMode");
-
+	
+	darkModeOn *= -1;
+	
+	if(darkMode == 1){
+		document.getElementById("text1").style.color = "#BB86FC";
+		document.getElementById("text2").style.color = "#BB86FC";
+	}
+	else{
+		document.getElementById("text1").style.color = "#BB4234";
+		document.getElementById("text2").style.color = "#BB4256";
+	}
+	
+	
 }
 
 
